1f556971e02bd4d51e6cbd148dcd6c3c84620339
Savyo Brenner <savyobrenner@gmail.com>
2021-12-27 00:39:57 +0100

    Initial Commit

---
+++ NarutoWiki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NarutoWiki
+//
+//  Created by Brenner on 27/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
